3f87448b07f0d4735ffce29ce89c9bf755eb653f
Maxim Kaplin <kaplin95@inbox.ru>
2020-06-06 09:22:33 +0300

    add timing for video

--- script.js
+++ script.js
@@ -26,11 +26,25 @@ function updatePlayIcon() {
 // Update progress and timestamp
 function updateProgress() {
     progress.value = (video.currentTime / video.duration) * 100;
+
+    // Get minutes
+    let mins = Math.floor(video.currentTime / 60);
+    if (mins < 10) {
+        mins = '0' + String(mins);
+    }
+
+    // Get seconds
+    let secs = Math.floor(video.currentTime % 60);
+    if (secs < 10) {
+        secs = '0' + String(secs);
+    }
+
+    timestamp.innerHTML = `${mins}:${secs}`;
 }
 
 // Set video time to progress 
 function setVideoProgress() {
-    video.currentTime = (progress.value * video.duration) /git  100
+    video.currentTime = (progress.value * video.duration) / 100
 }
 
 // Stop video
