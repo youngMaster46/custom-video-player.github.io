1486f5799178658184da59ca3ef3b41cbe2bc59d
Maxim Kaplin <kaplin95@inbox.ru>
2020-06-06 08:44:10 +0300

    add play and pause finctionality, and pause/play button via click

--- script.js
+++ script.js
@@ -4,6 +4,45 @@ const stop = document.getElementById('stop')
 const progress = document.getElementById('progress')
 const timestamp = document.getElementById('timestamp')
 
+// Play and pause video
+function toggleVideoStatus() {
+    if (video.paused) {
+        video.play()
+    }
+    else {
+        video.pause()
+    }
+}
+// Update play pause icon
+function updatePlayIcon() {
+    if (video.paused) {
+        play.innerHTML = '<i class="fa fa-play fa-2x"></i>';
+    }
+    else {
+        play.innerHTML = '<i class="fa fa-pause fa-2x"></i>';
+
+    }
+}
+// Update progress and timestamp
+function updateProgress() {
+    console.log('updateProgress');
+
+}
+
+// Set video time to progress 
+function setVideoProgress() {
+    console.log('setVideoProgress');
+
+}
+
+// Stop video
+function stopVideo() {
+    video.currentTime = 0;
+    video.pause();
+
+}
+
+// Events
 video.addEventListener('click', toggleVideoStatus);
 video.addEventListener('pause', updatePlayIcon);
 video.addEventListener('play', updatePlayIcon);
